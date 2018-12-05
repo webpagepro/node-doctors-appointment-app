278413b94883c43eb698df23dd698ea09e34838e
webpagepro <samuel@webpagepro.local>
2018-12-04 17:16:02 -0700

    doctor name

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -19,8 +19,12 @@ schedule: (req, res) => {
   knex('bookings')
   .where('booking_doctor_id', req.session.doctor_id)
     .then((booked) => {
-      console.log(booked);
-        res.render('bookings', {booked: booked});
+      knex('doctors')
+      .where('doctors.id', req.session.doctor_id)
+        .then((doc) =>  {
+      console.log(doc);
+        res.render('bookings', {booked: booked, doc: doc });
+        })
       })
     }
 
