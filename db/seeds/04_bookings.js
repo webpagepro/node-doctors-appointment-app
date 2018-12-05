b2d0984dffa669585bace59aaefe50d40f1a7dc4
webpagepro <samuel@webpagepro.local>
2018-12-05 09:57:32 -0700

    final re-commit seeds file

--- db/seeds/04_bookings.js
+++ db/seeds/04_bookings.js
@@ -5,12 +5,12 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('bookings').insert([
-        {patient_name: 'Erin Williams', booking_doctor_id: 1, booking_reason: 'aslkjfslfj', booking_details: 'kasfsad', booking_date: '11/24/2018', booking_confirmed: true, booking_complete: false},
-        {patient_name: 'Jessica DiRocco', booking_doctor_id: 2, booking_reason: 'aslkjfslfj', booking_details: 'kasfsad', booking_date: '11/24/2018', booking_confirmed: true, booking_complete: false},
+        {patient_name: 'Erin Williams', booking_doctor_id: 1, booking_reason: 'aslkjfslfj', booking_details: 'kasfsad', booking_date: '11/24/2018', booking_confirmed: true, booking_complete: true},
+        {patient_name: 'Jessica DiRocco', booking_doctor_id: 2, booking_reason: 'aslkjfslfj', booking_details: 'kasfsad', booking_date: '11/24/2018', booking_confirmed: false, booking_complete: false},
         {patient_name: 'Jeremy Richards', booking_doctor_id: 3, booking_reason: 'aslkjfslfj', booking_details: 'kasfsad', booking_date: '11/24/2018', booking_confirmed: true, booking_complete: false},
-        {patient_name: 'Mith Mitchell', booking_doctor_id: 1, booking_reason: 'aslkjfslfj', booking_details: 'kasfsad', booking_date: '11/24/2018', booking_confirmed: true, booking_complete: false},
+        {patient_name: 'Mith Mitchell', booking_doctor_id: 1, booking_reason: 'aslkjfslfj', booking_details: 'kasfsad', booking_date: '11/24/2018', booking_confirmed: true, booking_complete: true},
         {patient_name: 'Red Redding', booking_doctor_id: 1, booking_reason: 'aslkjfslfj', booking_details: 'kasfsad', booking_date: '11/24/2018', booking_confirmed: true, booking_complete: false}
-     
+
       ]);
     });
 };
