33e8234e51cca39c6b587bd9a289c4a5586a8daa
Samuel <webpagepro@aol.com>
2019-11-08 12:37:15 -0700

    db connection

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
       client: 'pg',
       connection: {
-        database: "doctors",
+        database: "",
         host: "localhost"
       },
       migrations: {
